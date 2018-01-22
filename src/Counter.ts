446447c228d5379568e7399fd0f9eef6226ce1c0
Roadagain <ringoh72@gmail.com>
2018-01-22 13:49:16 +0900

    Member changes to readonly sorted tuple array

--- src/Counter.ts
+++ src/Counter.ts
@@ -1,16 +1,18 @@
 export default class Counter {
-    private map: Map<string, number>;
+    readonly count: Array<[string, number]>;
 
     constructor(str: string) {
-        this.map = new Map<string, number>();
+        let map = new Map<string, number>();
         for (let c of str){
             //i hope setdefault to js map
             let currentCount = 0; // if the key isnt exist, count must be 0
-            if (this.map.has(c)){
+            if (map.has(c)){
                 // there is no worry about null: map has the key
-                currentCount = this.map.get(c) as number;
+                currentCount = map.get(c) as number;
             }
-            this.map.set(c, currentCount + 1);
+            map.set(c, currentCount + 1);
         }
+
+        this.count = [...map].sort(); // count array is sorted by chars
     }
 }
