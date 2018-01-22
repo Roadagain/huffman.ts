8137c17b505c47348cb43d52f1c78c5414bfa6c9
Roadagain <ringoh72@gmail.com>
2018-01-22 22:51:24 +0900

    Add some tests for HuffmanTreeBulider

---
+++ test/HuffmanTreeBuilder.test.ts
@@ -0,0 +1,34 @@
+///<reference path="../node_modules/@types/power-assert/index.d.ts" />
+///<reference path="../node_modules/@types/jasmine/index.d.ts" />
+
+import Counter from '../src/Counter';
+import HuffmanNode from '../src/HuffmanNode';
+import HuffmanTree from '../src/HuffmanNode';
+import HuffmanTreeBuilder from '../src/HuffmanTreeBuilder';
+import * as assert from 'assert';
+
+describe('huffman tree builder', () => {
+    it('construct a tree from a counter', () => {
+        const counter = new Counter('aabbc');
+        const tree = HuffmanTreeBuilder.fromCounter(counter);
+        const lexpected = new HuffmanNode('b', 2);
+        const rlexpected = new HuffmanNode('c', 1);
+        const rrexpected = new HuffmanNode('a', 2);
+        const rexpected = new HuffmanTree(rlexpected, rrexpected);
+        const expected = new HuffmanTree(lexpected, rexpected);
+
+        assert.equal(tree, expected, 'built tree is not actual');
+    });
+
+    it('construct a tree from a string', () => {
+        const str = 'aabbc';
+        const tree = HuffmanTreeBuilder.fromString(str);
+        const lexpected = new HuffmanNode('b', 2);
+        const rlexpected = new HuffmanNode('c', 1);
+        const rrexpected = new HuffmanNode('a', 2);
+        const rexpected = new HuffmanTree(rlexpected, rrexpected);
+        const expected = new HuffmanTree(lexpected, rexpected);
+
+        assert.equal(tree, expected, 'built tree is not actual');
+    });
+});
