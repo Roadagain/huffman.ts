c0ca08056bb7182ef85752ec3aa08cea6f602290
Roadagain <ringoh72@gmail.com>
2018-01-23 14:49:58 +0900

    Struct code symbol and body with compare function

---
+++ test/Code.test.ts
@@ -0,0 +1,38 @@
+///<reference path="../node_modules/@types/power-assert/index.d.ts" />
+///<reference path="../node_modules/@types/jasmine/index.d.ts" />
+
+import Code from '../src/Code';
+import * as assert from 'assert';
+
+describe('code', () => {
+    it('instance code', () => {
+        const symb = 'a';
+        const body = '01';
+        const code = new Code(symb, body);
+
+        assert.equal(code.symb, symb, 'symbol is not actual');
+        assert.equal(code.body, body, 'symbol is not actual');
+    });
+
+    it('compare code: less', () => {
+        const a = new Code('i', '11');
+        const b = new Code('n', '10');
+
+        assert.equal(Code.cmp(a, b), -1, 'less compare is not actual');
+    })
+
+    it('compare code: more', () => {
+        const a = new Code('w', '100');
+        const b = new Code('u', '110');
+
+        assert.equal(Code.cmp(a, b), 1, 'more compare is not actual');
+    })
+
+    it('compare code: equal', () => {
+        const a = new Code('o', '000');
+        const b = new Code('o', '000');
+
+        assert.deepEqual(a, b, 'equal compare is not actual');
+        assert.equal(Code.cmp(a, b), 0, 'equal compare is not actual');
+    })
+});
