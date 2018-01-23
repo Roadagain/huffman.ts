aac676e8f3d824f70030a9778fb958baa42f499c
Roadagain <ringoh72@gmail.com>
2018-01-23 09:38:17 +0900

    R.I.P. HuffmanNode

--- src/HuffmanNode.ts
+++
@@ -1,9 +0,0 @@
-export default class HuffmanNode {
-    readonly symb: string;
-    readonly count: number;
-
-    constructor(symb: string, count: number){
-        this.symb = symb;
-        this.count = count;
-    }
-}
