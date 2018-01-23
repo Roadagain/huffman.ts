ef8610be786b0f7253f5e4e2b098bad94e81ca9e
Roadagain <ringoh72@gmail.com>
2018-01-23 22:19:50 +0900

    Edit comments on Codebook

--- src/Codebook.ts
+++ src/Codebook.ts
@@ -6,15 +6,17 @@ module Codebook {
 
     export const fromHuffmanTree = (tree: HuffmanTree, prefix = ''): Code[] => {
         if (tree.left === undefined) {
+            // given tree is a leaf, so symb is not undefined
             return [new Code(tree.symb as string, prefix)];
         }
-        // children must not be undefined
+        // children is not be undefined
         const leftCodebook = fromHuffmanTree(tree.left, prefix + '0');
         const rightCodebook = fromHuffmanTree(tree.right as HuffmanTree, prefix + '1');
         return leftCodebook.concat(rightCodebook).sort(Code.cmp);
     };
 
     export const fromString = (str: string, prefix = ''): Code[] => {
+        // it can be a oneliner, but it will takes too long
         return fromHuffmanTree(HuffmanTreeBuilder.fromString(str)); // sorted
     };
 
