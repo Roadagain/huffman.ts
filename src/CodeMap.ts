52b5c4e0ee942a549edfded114dcd57eb70fdf0a
Roadagain <ringoh72@gmail.com>
2018-01-24 14:09:01 +0900

    Fix syntax errors in function allocating

--- src/CodeMap.ts
+++ src/CodeMap.ts
@@ -4,12 +4,12 @@ import Code from './Code';
 module CodeMap {
     type CodeMap = Map<string, string>;
 
-    export const fromCodebook(str: string, codebook: Code[]): CodeMap => {
+    export const fromCodebook = (str: string, codebook: Code[]): CodeMap => {
         const tuplized = codebook.map((c: Code): [string, string] => [c.symb, c.body]);
         return new CodeMap(tuplized);
     };
 
-    export const fromString(str: string): CodeMap => {
+    export const fromString = (str: string): CodeMap => {
         // As usual, onelinerize will takes too long
         return fromCodebook(str, Codebook.fromString(str));
     }
