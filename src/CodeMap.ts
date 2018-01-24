3b436195cd6614add4f2ccf9880b8d0fad26ea01
Roadagain <ringoh72@gmail.com>
2018-01-24 13:31:46 +0900

    Implement mapping a codebook

---
+++ src/CodeMap.ts
@@ -0,0 +1,16 @@
+import Codebook from './Codebook';
+import Code from './Code';
+
+module CodeMap {
+    type CodeMap = Map<string, string>;
+
+    export const fromCodebook(str: string, codebook: Code[]): CodeMap => {
+        const tuplized = codebook.map((c: Code): [string, string] => [c.symb, c.body]);
+        return new CodeMap(tuplized);
+    };
+
+    export const fromString(str: string): CodeMap => {
+        // As usual, onelinerize will takes too long
+        return fromCodebook(str, Codebook.fromString(str));
+    }
+}
